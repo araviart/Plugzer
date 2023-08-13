b48c42e4befb329be709c3bc5479cff827725321
Steve <steve.mothmora@gmail.com>
2023-08-13 16:02:25 +0200

    coding the book 'create' function

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -1,9 +1,27 @@
-const booksModel = require("../models/books.model");
+const Books = require("../models/books.model");
 
 
 // Create a book page
 exports.create = async (req, res) => {
-
+  const userId = req.auth.userId;
+  const title = req.body.title;
+  const author = req.body.author;
+  const imageUrl = req.body.imageUrl;
+  const year = req.body.year;
+  const genre = req.body.genre;
+  try {
+    const book = await Books.create({
+      userId,
+      title,
+      author,
+      imageUrl,
+      year,
+      genre
+    })
+    return res.status(201).json(book)
+  } catch (error) {
+    res.status(500).json({ error: 'Something went wrong' })
+  }
 };
 
 // Rate a book
@@ -18,7 +36,8 @@ exports.delete = async (req, res) => {
 
 // Display every books
 exports.getAll = async (req, res) => {
-
+  const books = await booksModel.findAll();
+  return res.status(200).json(books)
 };
 
 // Display only one book
