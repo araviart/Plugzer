8842b12e4c0401cca5665c616762171f82f2e73b
Steve <steve.mothmora@gmail.com>
2023-08-16 01:30:09 +0200

    Done with every issues encountered with CRUD

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -8,9 +8,10 @@ const multer = require('../middlewares/multer-config');
 
 router.post('/', auth, booksControllers.create);
 router.post('/:id/rating', auth, booksControllers.rate);
+router.get('/bestrating', booksControllers.bestRatings);
 router.get('/', booksControllers.getAll);
 router.get('/:id', booksControllers.getOne);
-router.get('/bestrating', booksControllers.bestRatings);
+
 router.put('/:id', auth, multer, booksControllers.modify);
 router.delete('/:id', auth, booksControllers.delete);
 
