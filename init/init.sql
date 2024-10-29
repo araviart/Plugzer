cf4a2c5fe873d04f7915600fa35a50befca4b2b5
seb_grtd <sebastien.gratade@etu.univ-orleans.fr>
2024-10-29 16:22:38 +0100

    bd

--- init/init.sql
+++ init/init.sql
@@ -1,6 +1,36 @@
-create table todo
-(
-    id        int          not null auto_increment primary key,
-    title     varchar(255) not null,
-    completed boolean default false
+CREATE TABLE utilisateur (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    mdp VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE storage (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    utilisateur_id INT NOT NULL,
+    nom_fichier VARCHAR(255) NOT NULL,
+    taille_fichier INT NOT NULL, -- taille en octets
+    FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id)
+);
+DELIMITER //
+
+CREATE FUNCTION peut_ajouter_fichier(utilisateur_id INT, taille_fichier INT) RETURNS BOOLEAN
+DETERMINISTIC
+BEGIN
+    DECLARE espace_total INT;
+
+    SELECT IFNULL(SUM(taille_fichier), 0) INTO espace_total
+    FROM storage
+    WHERE utilisateur_id = utilisateur_id;
+
+    RETURN (espace_total + taille_fichier <= 2 * 1024 * 1024 * 1024);
+END //
+
+DELIMITER ;
+CREATE TABLE lien_fichier (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fichier_id INT NOT NULL,
+    lien VARCHAR(255) NOT NULL,
+    date_expiration DATE NOT NULL DEFAULT (CURRENT_DATE + INTERVAL 1 DAY),
+    FOREIGN KEY (fichier_id) REFERENCES storage(id)
 );
